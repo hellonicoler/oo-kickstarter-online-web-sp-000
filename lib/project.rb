4db9c41bfac51ed4c030d89cecc7c7f16daea197
Nicole Rothenay <nthorn1211@gmail.com>
2020-01-03 15:37:59 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,7 @@ end
   
 def add_backer(backer)
   @backers << backer
-  project.add_backer(self) unless project.backers.include?(self)
+  backer.back_project(self) unless backer.backed_projects.include?(self)
 end
   
 end
